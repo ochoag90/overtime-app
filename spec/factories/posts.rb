69f0ea032e385d724b42b8d01b4f67e8ef5d0be1
Gabe Ochoa <gochoa5190@gmail.com>
2018-04-09 16:05:57 -0400

    Implemented enum for status into posts for approval workflow

--- spec/factories/posts.rb
+++ spec/factories/posts.rb
@@ -2,14 +2,12 @@ FactoryBot.define do
 	factory :post do
 		date Date.today
 		rationale "Some Rationale"
-		# user_id 3
 		user
 	end 
 
 	factory :second_post, class: "Post" do
 		date Date.yesterday
 		rationale "Some Content"
-		# user_id 3
 		user
 	end
 end 
