2488490d8a848ae3d6f558936ef50f40ee4ce9b2
Gabe Ochoa <gochoa5190@gmail.com>
2018-04-17 22:46:40 -0400

    Adding the backend data to display to the audit log index view

--- db/seeds.rb
+++ db/seeds.rb
@@ -32,7 +32,7 @@ end
 puts "100 Posts have been created"
 
 100.times do |audit_log|
-	AuditLog.create!(user_id: @user.id, status: 0, start_date: (Date.today - 2.days))
+	AuditLog.create!(user_id: @user.id, status: 0, start_date: (Date.today - 6.days))
 end 
 
 puts "100 AuditLogs have been created"
---
+++ spec/features/audit_log_spec.rb
@@ -0,0 +1,30 @@
+require 'rails_helper'
+
+describe 'Auditlog Feature' do
+	describe 'index' do
+		before do
+			admin_user = FactoryBot.create(:admin_user)
+      		login_as(admin_user, :scope => :user)
+      		FactoryBot.create(:audit_log)
+		end 
+
+			it 'has a index page that can be reached' do
+				visit audit_logs_path
+				expect(page.status_code).to eq(200)
+			end
+
+			it 'renders audit log content' do
+				visit audit_logs_path
+				expect(page).to have_content(/SNOW/)
+			end 
+
+			xit 'cannot be accesed by non admin users' do
+
+			end 
+	end
+end
+
+
+
+
+	
